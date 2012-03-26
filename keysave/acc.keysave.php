896d50ba8c5cd5613fb93b9e1c8f2becfabef822
Blis Web Agency <sam@blis.net.au>
2012-03-27 09:03:00 +1030

    Initial commit

---
+++ keysave/acc.keysave.php
@@ -0,0 +1,60 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ctrlstpl_acc {
+
+	var $name		= 'Ctrl+s hot key for Publish / Edit';
+	var $id			= 'keysave';
+	var $version		= '1.0';
+	var $description	= 'Because both Mac and Windows have a CTRL key';
+	var $sections		= array();
+	
+	/**
+	 * Constructor
+	 */
+	function Keysave_acc()
+	{
+		$this->EE =& get_instance();
+	}
+	/**
+	 * Set Sections
+	 *
+	 * Set content for the accessory
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	function set_sections()
+	{
+		
+		$this->EE->load->library('javascript');
+
+		$str = <<<END
+		
+		 $("#ctrlstpl.accessory").remove();
+         $("#accessoryTabs").find("a.ctrlstpl").parent("li").remove();
+		
+		var isCtrl = false;
+		$(document).keyup(function (e) {
+			if(e.which == 17 || e.which == 224) isCtrl=false;
+		}).keydown(function (e) {
+
+			if(e.which == 17 || e.which == 224) isCtrl=true;
+
+			if(e.which == 79 && isCtrl == true) {
+				//run code for CTRL+S -- ie, save!
+				$('#submit_button').trigger('click');
+				return false;
+			}
+		});
+END;
+		$this->EE->javascript->output($str);
+		
+		if($this->EE->input->get('D') == 'cp' AND $this->EE->input->get('C') == 'addons_accessories')
+        {
+           $this->EE->db->where('class', 'Ctrlstpl_acc');
+           $this->EE->db->update('accessories', array('controllers' => 'design'));
+        }
+		
+	}
+}
+// END CLASS
